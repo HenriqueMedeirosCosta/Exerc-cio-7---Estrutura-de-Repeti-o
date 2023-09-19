58cd9cc47bb68653fc61edfc72545ab3ae242aa8
HenriqueMedeirosCosta <132111448+HenriqueMedeirosCosta@users.noreply.github.com>
2023-09-18 22:27:15 -0300

    Create exercício2.c

---
+++ exercício2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<string.h>
+
+main() {
+*int n, i=0;
+   
+  
+  printf("digite um numero inteiro positivo\n");
+  scanf("%d",&n);
+
+  while(n>=i){
+     printf("\n%d",n);
+    n--;*/
+  }
