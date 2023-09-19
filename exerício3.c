c9cf00fc3197f8eb108832a66d1e63a2bcf4f2ee
HenriqueMedeirosCosta <132111448+HenriqueMedeirosCosta@users.noreply.github.com>
2023-09-18 22:29:13 -0300

    Create exerício3.c

---
+++ exerício3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<string.h>
+
+main() {int n, i=1, impar=1;
+   
+  printf("digite um numero inteiro \n");
+  scanf("%d",&n);
+
+  while(i<= n){
+
+    printf("\n%d",impar);
+
+  impar+=2;
+    i++;
+    
+  }
