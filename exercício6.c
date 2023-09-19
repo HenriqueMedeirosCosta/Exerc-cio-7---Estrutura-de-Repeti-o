14f56c3800473b39a593418821cf6a7c1801eeef
HenriqueMedeirosCosta <132111448+HenriqueMedeirosCosta@users.noreply.github.com>
2023-09-18 22:31:43 -0300

    Create exercício6.c

---
+++ exercício6.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include<string.h>
+
+main() {
+int fruta, novoPedido, total;
+  float valor;
+  char p[20], a[20], m[20];
+
+  
+    printf("Faça seu pedido escolhendo a partir do codigo: \n 1- Abacaxi: R$5,00 \n 2- Maça:    R$1,00 \n 3- Pera:    R$4,00 \n");
+  scanf("%d", &fruta);
