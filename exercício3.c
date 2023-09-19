79b0d5a011fab38c9943a14196b44480a228d359
HenriqueMedeirosCosta <132111448+HenriqueMedeirosCosta@users.noreply.github.com>
2023-09-18 22:27:54 -0300

    Create exercício3.c

---
+++ exercício3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<string.h>
+
+main() {
+int multiplos=3,m=2, i=1 , n=5 ;
+
+  while(i<=n){
+
+    printf("\n%d",multiplos);
+
+    multiplos = m * 3;
+    m++;
+    i++;
+  }
